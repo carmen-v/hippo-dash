79c013905a7077fdac135e9320eaab03ea095f90
carmen-v <carmen@carmen.pro>
2025-01-05 17:05:50 +0100

    added menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -11,14 +12,18 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] xl:w-[14%] bg-gray-800">
-        <Link href= "/" className="flex items-center justify-center lg:justify-start gap-2 p-4">
+      <div className="w-[14%] md:w-[8%] xl:w-[14%]  p-4 bg-[#17080d]">
+        <Link href= "/" className="flex items-center justify-center lg:justify-start gap-2">
           <Image src="/hippo-logo.svg" alt="Logo" width={32} height={32}/>
           <span className="hidden lg:block">HippoDash</span>
         </Link>
+        <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-orange-800">r</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,24 +1,101 @@
-import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
 
-interface MenuItem {
-    label: string;
-    link: string;
-}
+const menuItems = [
+  {
+    title: "MENU",
+    items: [
+      {
+        icon: "/home.png",
+        label: "Home",
+        href: "/",
+        visible: ["yoohoos", "buni", "rob"],
+      },
+      {
+        icon: "/yoohoos.png",
+        label: "Yoohoos",
+        href: "/list/yoohoos",
+        visible: ["yoohoos", "buni", "rob"],
+      },
+      {
+        icon: "/rob.png",
+        label: "Rob",
+        href: "/list/rob",
+        visible: ["yoohoos", "buni", "rob"],
+      },
+      {
+        icon: "/buni.png",
+        label: "Buni",
+        href: "/list/teachers",
+        visible: ["yoohoos", "buni", "rob"],
+      },
+      {
+        icon: "/shopping-list.png",
+        label: "Shopping List",
+        href: "/list/shopping-list",
+        visible: ["yoohoos", "buni", "rob"],
+      },
+      {
+        icon: "/calendar.png",
+        label: "Events",
+        href: "/list/events",
+        visible: ["yoohoos", "buni", "rob"],
+      },
+      {
+        icon: "/message.png",
+        label: "Messages",
+        href: "/list/messages",
+        visible: ["yoohoos", "buni", "rob"],
+      },
+      {
+        icon: "/announcement.png",
+        label: "Announcements",
+        href: "/list/announcements",
+        visible: ["yoohoos", "buni", "rob"],
+      },
+    ],
+  },
+  {
+    title: "OTHER",
+    items: [
+      {
+        icon: "/profile.png",
+        label: "Profile",
+        href: "/profile",
+         visible: ["yoohoos", "buni", "rob"],
+      },
+      {
+        icon: "/setting.png",
+        label: "Settings",
+        href: "/settings",
+         visible: ["yoohoos", "buni", "rob"],
+      },
+      {
+        icon: "/logout.png",
+        label: "Logout",
+        href: "/logout",
+         visible: ["yoohoos", "buni", "rob"],
+      },
+    ],
+  },
+];
 
-interface MenuProps {
-    items: MenuItem[];
-}
-
-const Menu: React.FC<MenuProps> = ({ items }) => {
-    return (
-        <ul>
-            {items.map((item, index) => (
-                <li key={index}>
-                    <a href={item.link}>{item.label}</a>
-                </li>
-            ))}
-        </ul>
-    );
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div key={i.title} className="flex flex-col gap-2">
+          <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+          {i.items.map((item) => (
+            <Link key={item.label} href={item.href} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+              <Image src={item.icon} alt={item.label} width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;
